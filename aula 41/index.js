f8513cdee7de25315394af494e927031f6a7f5a2
patricksousamacedo <patricksouzamacedo@gmail.com>
2021-12-01 09:09:30 -0300

    commit

---
+++ aula 41/index.js
@@ -0,0 +1,4 @@
+// Escreva uma função que recebe 2 númerose retorne o maior deles
+
+const max = (x, y) => x > y ? x : y;
+console.log(max(1, 2))
---
+++ aula 42/index.js
@@ -0,0 +1,5 @@
+// note que o proprio retorno será um verdadeiro ou falso
+
+const ePaisagem = (largura, altura) => largura > altura
+
+console.log(ePaisagem(1080, 1920));
